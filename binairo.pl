b312307bbf312156d63b7dbea46eca7dfc01cafc
Indigoing-Gone <155276295+Indigoing-Gone@users.noreply.github.com>
2025-12-04 17:27:48 -0500

    messin around with length

--- binairo.pl
+++ binairo.pl
@@ -5,11 +5,10 @@
 % 1 1
 % 0 1
 binairo(Rows, Dimension) :- 
-    length(Rows, Dimension),
-    maplist(same_length(Dimension), Rows),  % ensure all lists are of length Dimension
-    transpose(Rows, Cols),                  % creates list of lists to represent columns
-    maplist(same_length(Dimension), Cols),
-
+    length(Rows, Dimension),                % number of rows is equal to the Dimension
+    maplist(same_length(Dimension), Rows),  % ensure all lists are of length Dimension / number of columns is equal to the Dimension
+    transpose(Rows, Cols).                  % creates list of lists to represent columns
+    
     % constrain rows and columns to not allow three in a row of 1s or 0s
     maplist(constrain_three(Row), Rows),
     maplist(constrain_three(Col), Cols).
