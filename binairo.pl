ee1adbb4fb927eeb264cf81961f8e33fa45fbd8f
Max Dyer <dyer.max.d@gmail.com>
2025-12-06 14:12:02 -0500

    add comments

--- binairo.pl
+++ binairo.pl
@@ -1,13 +1,13 @@
 :- use_module(library(clpfd)).
 
-% solve a board(Rows) based on rules of binairo on a Dimension by Dimension grid
 % Rows is a list of list where [[1,1],[0,1]] would translate to a board of
 % 1 1
 % 0 1
 
-test() :- 
-    puzzle(2, Rows),
-    binairo(Rows, 12, Solution),
+% test with given puzzles
+test(N, M) :- 
+    puzzle(N, Rows),
+    binairo(Rows, M, Solution),
     maplist(portray_clause, Solution).
 
 puzzle(1, [
@@ -16,7 +16,6 @@ puzzle(1, [
     [_, _, _, _],
     [_, 1, _, _]
 ]).
-
 
 puzzle(2, [
     [_, 0, _, 0, _, _, _, _, _, 0, _, _],
@@ -33,12 +32,12 @@ puzzle(2, [
     [_, _, _, _, 0, _, _, _, _, _, _, _]
 ]).
 
-
+% solve a board(Rows) based on rules of binairo on a Dimension by Dimension grid
 binairo(Rows, Dimension, Solution) :- 
     append(Rows, Vars), Vars ins 0..1,          % all variables are either 0 or 1
-    length(Rows, Dimension),                % number of rows is equal to the Dimension
-    maplist(same_length(Dimension), Rows),  % ensure all lists are of length Dimension / number of columns is equal to the Dimension
-    transpose(Rows, Cols),                  % creates list of lists to represent columns
+    length(Rows, Dimension),                    % number of rows is equal to the Dimension
+    maplist(same_length(Dimension), Rows),      % ensure all lists are of length Dimension / number of columns is equal to the Dimension
+    transpose(Rows, Cols),                      % creates list of lists to represent columns
 
     % constrain rows and columns to not allow three in a row of 1s or 0
     triplet_constraint(Rows, Cols),
@@ -57,10 +56,11 @@ triplet_constraint(Rows, Cols) :-
     maplist(no_triplets, Rows),
     maplist(no_triplets, Cols).
 
+% ensure no three of a kind in a row
 no_triplets([_, _]).                    % lists of length 2 cannot have triplets
 no_triplets([A, B, C | Remainder]) :- 
-    A + B + C #\= 0,                    % not three 0s
-    A + B + C #\= 3,                    % not three 1s
+    A + B + C #\= 0,                    % not 000
+    A + B + C #\= 3,                    % not 111
     no_triplets([B, C | Remainder]).    % remove first element and check rest of list
 
 % constrain board so all rows and columns have the same numbers of 1s and the same numbers of 0s
@@ -68,11 +68,13 @@ equality_constraint(Rows, Cols) :-
     maplist(limit_half, Rows),
     maplist(limit_half, Cols).
 
+% ensure equal count of 0s and 1s
 limit_half(List) :-
     count(0, List, Count0),
     count(1, List, Count1),
     Count0 #= Count1.
 
+% count the number of 0s and the number of 1s
 count(_, [], 0).
 count(Var, [X|Remainder], Count) :- 
     X #= Var,
@@ -87,11 +89,13 @@ uniqueness_constraint(Rows, Cols) :-
     unique_list_of_lists(Rows),
     unique_list_of_lists(Cols).
 
+% ensure CurrentList is not equal to any other list in list of lists
 unique_list_of_lists([_]).
 unique_list_of_lists([CurrentList | Remainder]) :-
     maplist(lists_not_equal(CurrentList), Remainder),   % check CurrentList against the remaining lists
     unique_list_of_lists(Remainder).                    % recurse through the rest of the lists
 
+% ensure elements of list are not equal to another list
 lists_not_equal([], []) :- false.
 lists_not_equal([A | As], [B | Bs]) :-
     A #= B,
